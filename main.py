3590a62a79a93cab3d73a50b2cb2878e59f12358
VELYX <vanya.yepez@gmail.com>
2025-07-29 17:53:55 -0600

    Initializing project main.py code

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(book_path):
+    with open (book_path) as f:
+        book_text = f.read()
+        return book_text
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+main()
