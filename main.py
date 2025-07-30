ce344a1419891175bdc774390817fd1c0e18c594
VELYX <vanya.yepez@gmail.com>
2025-07-29 18:45:30 -0600

    Cleaning up the get_book_text function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def get_book_text(book_path):
     with open (book_path) as f:
         book_text = f.read()
-        return book_text
+    return book_text
 
 def main():
     print(get_book_text("books/frankenstein.txt"))
