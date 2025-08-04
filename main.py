4a219a66678e11bfd07de3c786c80c9c73e48895
VELYX <vanya.yepez@gmail.com>
2025-08-03 21:20:19 -0600

    Adding word counter

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+def book_word_counter(book_path):
+    return len(get_book_text(book_path).split())
+
 def get_book_text(book_path):
     with open (book_path) as f:
         book_text = f.read()
     return book_text
 
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    # print(get_book_text("books/frankenstein.txt"))
+    print(book_word_counter("books/frankenstein.txt"), "words found in the document")
 
 main()
