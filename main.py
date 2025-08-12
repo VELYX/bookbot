7847622bebb1651edac4ffea4144da9f15f34653
VELYX <vanya.yepez@gmail.com>
2025-08-12 01:04:35 -0600

    Implementing character_counter, errors to fix [2]

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from stats import book_word_counter
+from stats import character_counter
 
 def main():
     # print(get_book_text("books/frankenstein.txt"))
     print(book_word_counter("books/frankenstein.txt"), "words found in the document")
+    char_counts = character_counter("books/frankenstein.txt")
+    print(char_counts)
 
 main()
--- stats.py
+++ stats.py
@@ -7,3 +7,13 @@ def book_word_counter(book_path):
     words = get_book_text(book_path).split()
     return len(words)
 
+def character_counter(book):
+    char_counts = {}
+    lowercase_char = character.lower()
+    for lowercase_char in book:
+        if character in char_count:
+            char_counts[lowercase_char] += 1
+        else:
+            char_counts[lowercase_char] = 1
+        return char_counts
+            
